f94e5dad4d0f590ccd2a3316a1bad31789424765
zhiwenli <lizhiwenzg@gmail.com>
2021-07-24 12:25:07 -0700

    fix redux compose bug

--- cupcake-shop-client/src/store/configureStore.js
+++ cupcake-shop-client/src/store/configureStore.js
@@ -4,9 +4,9 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 
 const composeEnhancers =
-  process.env.NODE_ENV === 'development'
+  (process.env.NODE_ENV === 'development'
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose;
+    : null) || compose;
 
 const configureStore = () => {
   return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
